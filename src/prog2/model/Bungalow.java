ac7ad109d7abb931196b3ff62a2f026bdc7dcf3f
anni007199 <anniqixu@gmail.com>
2026-03-05 12:36:14 +0100

    Fet una mica

--- src/prog2/model/Bungalow.java
+++ src/prog2/model/Bungalow.java
@@ -12,8 +12,8 @@ public abstract class Bungalow extends Casa {
     private boolean aireFred;
 
     // Constructor
-    public Bungalow(String nom, String id, long estadaMinimaALTA_, long estadaMinimaBAIXA_, Mida mida, int numHabitacions, int placesPersones, int placesParking, boolean terrassa, boolean tele, boolean aireFred){
-        super(nom, id, estadaMinimaALTA_, estadaMinimaBAIXA_, mida, numHabitacions, placesPersones);
+    public Bungalow(String nom, String id, long estadaMinimaALTA_, long estadaMinimaBAIXA_, Mida mida, int numHabitacions, int placesPerPersones, int placesParking, boolean terrassa, boolean tele, boolean aireFred){
+        super(nom, id, estadaMinimaALTA_, estadaMinimaBAIXA_, mida, numHabitacions, placesPerPersones);
         this.placesParking = placesParking;
         this.terrassa = terrassa;
         this.tele = tele;
@@ -28,6 +28,6 @@ public abstract class Bungalow extends Casa {
 
     @Override
     public String toString(){
-        return super.toString()+" Bungalow{places de Parking="+placesParking+", té terrassa?"+terrassa+", té televisió? "+ tele+ ", té aire fred?"+aireFred+"}";
+        return super.toString()+" Bungalow{places de Parking="+placesParking+", té terrassa="+terrassa+", té televisió= "+ tele+ ", té aire fred="+aireFred+"}";
     }
 }
