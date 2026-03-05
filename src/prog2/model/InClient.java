237509cf33eae6f9cb218257c65cf7e81b558813
davbu <davbusmar@gmail.com>
2026-03-05 13:13:28 +0100

    Fet

--- src/prog2/model/InClient.java
+++ src/prog2/model/InClient.java
@@ -1,14 +1,34 @@
 package prog2.model;
 
-public interface InClient {
+public class Client implements InClient {
 
-    public String getNom();
+    private String nom;
+    private String dni;
 
-    public String getDni();
+    // Constructor de la classe Client.
+    public Client(String nom, String dni) {
+        this.nom = nom;
+        this.dni = dni;
+    }
 
-    public void setNom(String nom);
+    public String getNom() {
+        return nom;
+    }
 
-    public void setDni(String dni);
+    public String getDni() {
+        return dni;
+    }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String toString() {
+        return "Nom=" + nom + ", DNI=" + dni;
+    }
 
 }
