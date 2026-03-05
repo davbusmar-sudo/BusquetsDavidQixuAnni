967cc9dde7f2b0f4c5debf331dcf8cbc46755df9
anni007199 <anniqixu@gmail.com>
2026-03-05 13:09:47 +0100

    Fet

--- src/prog2/model/Parcela.java
+++ src/prog2/model/Parcela.java
@@ -4,16 +4,31 @@ package prog2.model;
 public abstract class Parcela extends Allotjament {
     // Atributs
     // la mida de la parcel·la en metres quadrats
-    private double mida;
+    private float mida;
     // 'boolean' per indicar si té punt de connexió elèctrica
     private boolean connexioElectrica;
 
     // Constructor
-    public Parcela (String nom, String id, long estadaMinimaALTA_, long estadaMinimaBAIXA_, double mida, boolean connexioElectrica){
+    public Parcela (String nom, String id, long estadaMinimaALTA_, long estadaMinimaBAIXA_, float mida, boolean connexioElectrica){
         // cridar al constructor de la classe 'Allotjament' per inicialitzar els atributs heretats
         super(nom,id,estadaMinimaALTA_,estadaMinimaBAIXA_);
         // Atributs específics de Parcel·la
         this.mida = mida;
+        this.connexioElectrica = connexioElectrica;
+    }
+
+    // getters
+    public float getMida(){
+        return mida;
+    }
+    public boolean getConnexioElectrica(){
+        return connexioElectrica;
+    }
+    // setters
+    public void setMida(float mida){
+        this.mida = mida;
+    }
+    public void setConnexioElectrica(boolean connexioElectrica){
         this.connexioElectrica = connexioElectrica;
     }
     /*
