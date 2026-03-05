ef8770a950329ed1a3bf1351396cfe8d37ac4f28
anni007199 <anniqixu@gmail.com>
2026-03-05 13:20:21 +0100

    Fet

--- src/prog2/model/BungalowPremium.java
+++ src/prog2/model/BungalowPremium.java
@@ -4,30 +4,47 @@ package prog2.model;
 public abstract class BungalowPremium extends Bungalow {
     // Atributs
     // 'boolean' per indicar si tenen o no
-    private boolean llencolsTovalloles;
-    private boolean wifi;
+    private boolean serveisExtra;
+    private String codiWifi;
 
     // Constructor
-    public BungalowPremium(String nom, String id, long estadaMinimaALTA_, long estadaMinimaBAIXA_, Mida mida, int numHabitacions, int placesPerPersones, int placesParking, boolean terrassa, boolean tv, boolean aireFred, boolean llencolsTovalloles, boolean wifi){
-        super(nom, id, estadaMinimaALTA_, estadaMinimaBAIXA_, mida, numHabitacions, placesPerPersones, placesParking, terrassa, tv, aireFred);
-        this.llencolsTovalloles = llencolsTovalloles;
-        this.wifi = wifi;
+    public BungalowPremium(String nom, String id, long estadaMinimaALTA_, long estadaMinimaBAIXA_, Mida mida, int habitacions, int placesPersones, int placesParquing, boolean terrassa, boolean tv, boolean aireFred, boolean serveisExtra, String codiWifi){
+        super(nom, id, estadaMinimaALTA_, estadaMinimaBAIXA_, mida, habitacions, placesPersones, placesParquing, terrassa, tv, aireFred);
+        this.serveisExtra = serveisExtra;
+        this.codiWifi = codiWifi;
+    }
+
+    // getters
+    public boolean getServeisExtra(){
+        return serveisExtra;
+    }
+    public String getCodiWifi(){
+        return codiWifi;
+    }
+    // setters
+    public void setServeisExtra(boolean serveisExtra){
+        this.serveisExtra = serveisExtra;
+    }
+    public void setCodiWifi(String codiWifi){
+        this.codiWifi = codiWifi;
     }
     // Mètode per saber si el codi Wifi assignat té entre 8 i 16 caràcters.
     public boolean codiWifiCorrecte(String codi){
         if (codi.length() >= 8 && codi.length() <= 16){
-            return wifi;
+            return true;
+        } else {
+            return false;
         }
     }
     // Bungalow Premium té un funcionament correcte si té aire fred
     //i el codi Wifi assignat té entre 8 i 16 caràcters.
     @Override
     public boolean correcteFuncionament(){
-        return aireFred;
+        return getAireFred() && codiWifiCorrecte(codiWifi);
     }
 
     @Override
     public String toString(){
-        return super.toString()+" BungalowPrenium{llençolsTovalloles = "+llencolsTovalloles+", wifi="+wifi+"}";
+        return super.toString()+" BungalowPrenium{llençolsTovalloles = "+serveisExtra+", wifi="+codiWifi+"}";
     }
 }
