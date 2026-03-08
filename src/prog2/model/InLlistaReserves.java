b939e16a4ce4a4a7bd8e91ea9d4971dda6bbba69
davbu <davbusmar@gmail.com>
2026-03-08 13:51:04 +0100

    Fet

--- src/prog2/model/InLlistaReserves.java
+++ src/prog2/model/InLlistaReserves.java
@@ -3,7 +3,6 @@ package prog2.model;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import prog2.vista.ExcepcioReserva;
 
 
@@ -25,41 +24,9 @@ public interface InLlistaReserves {
      * @param dataSortida
      * @throws ExcepcioReserva 
      */
-     void afegirReserva(Allotjament allotjament, Client client, LocalDate dataEntrada, LocalDate dataSortida) throws ExcepcioReserva;
-
-            if (!isEstadaMinima(allotjament, dataEntrada, dataSortida)) {
-                throw new ExcepcioReserva("Les dates sol·licitades pel client " + client.getNom() + " amb DNI: " + client.getDni() + " no compleixen l'estada mínima per l'allotjament amb identificador " + allotjament.getId() + ".");
-            }
-            if (!allotjamentDisponible(allotjament, dataEntrada, dataSortida)) {
-                throw new ExcepcioReserva("L'allotjament amb identificador " + allotjament.getId() + " no està disponible en la data demanada " + dataEntrada + " pel client " + client.getNom() + " amb DNI: " + client.getDni() + ".");
-            }
-
-            Reserva novaReserva = new Reserva(allotjament, client, dataEntrada, dataSortida);
-            llista.add(novaReserva);
+    void afegirReserva(Allotjament allotjament, Client client, LocalDate dataEntrada, LocalDate dataSortida) throws ExcepcioReserva;
 
 
+    int getNumReserves();
 
-    }
-
-}
-
-private boolean isEstadaMinima(Allotjament allotjament, LocalDate dataEntrada, LocalDate dataSortida) {
-    long estada = ChronoUnit.DAYS.between(dataEntrada, dataSortida);
-    InAllotjament.Temp temp = Camping.getTemporada(dataEntrada)
-    return estada >= allotjament.getEstadaMinima(temp);
-}
-
-
-private boolean allotjamentDisponible(Allotjament allotjament, LocalDate dataEntrada, LocalDate dataSortida) {
-    for (Reserva r : llista) {
-        // Si és el mateix allotjament, mirem si les dates se solapen
-        if (r.getAllotjament().getId().equals(allotjament.getId())) {
-            // Hi ha solapament si (Entrada1 < Sortida2) i (Sortida1 > Entrada2)
-            if (dataEntrada.isBefore(r.getDataSortida()) && dataSortida.isAfter(r.getDataEntrada())) {
-                return false; // No disponible
-            }
-        }
-    }
-    return true; // Disponible
-}
 }
--- src/prog2/model/LlistaReserves.java
+++ src/prog2/model/LlistaReserves.java
@@ -1,6 +1,9 @@
 package prog2.model;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import prog2.vista.ExcepcioReserva;
 
 public class LlistaReserves implements InLlistaReserves {
 
@@ -10,13 +13,44 @@ public class LlistaReserves implements InLlistaReserves {
         this.llista = new ArrayList<>();
     }
 
-    /**
-     * Retorna el número de reserves de la llista.
-     *
-     * @return el número de reserves.
-     */
+    @Override
     public int getNumReserves() {
         return this.llista.size();
     }
 
+    @Override
+    public void afegirReserva(Allotjament allotjament, Client client, LocalDate dataEntrada, LocalDate dataSortida) throws ExcepcioReserva {
+
+        // 1. Comprovar estada mínima
+        if (!isEstadaMinima(allotjament, dataEntrada, dataSortida)) {
+            throw new ExcepcioReserva("Les dates sol·licitades pel client " + client.getNom() + " amb DNI: " + client.getDni() + " no compleixen l'estada mínima per l'allotjament amb identificador " + allotjament.getId() + ".");
+        }
+
+        // 2. Comprovar disponibilitat
+        if (!allotjamentDisponible(allotjament, dataEntrada, dataSortida)) {
+            throw new ExcepcioReserva("L'allotjament amb identificador " + allotjament.getId() + " no està disponible en la data demanada " + dataEntrada + " pel client " + client.getNom() + " amb DNI: " + client.getDni() + ".");
+        }
+
+        // 3. Si és disponible;
+        Reserva novaReserva = new Reserva(allotjament, client, dataEntrada, dataSortida);
+        llista.add(novaReserva);
+    }
+
+    // metodos per fer les comprovacions
+    private boolean isEstadaMinima(Allotjament allotjament, LocalDate dataEntrada, LocalDate dataSortida) {
+        long estada = ChronoUnit.DAYS.between(dataEntrada, dataSortida); // [cite: 164]
+        InAllotjament.Temp temp = Camping.getTemporada(dataEntrada); // [cite: 134]
+        return estada >= allotjament.getEstadaMinima(temp);
+    }
+
+    private boolean allotjamentDisponible(Allotjament allotjament, LocalDate dataEntrada, LocalDate dataSortida) {
+        for (Reserva r : llista) {
+            if (r.getAllotjament_().getId().equals(allotjament.getId())) {
+                if (dataEntrada.isBefore(r.getDataSortida()) && dataSortida.isAfter(r.getDataEntrada())) {
+                    return false;
+                }
+            }
+        }
+        return true; // Està disponible
+    }
 }
